3771280992266f3aa73d1ecc24ad50ebea27cf5c
Philipp Schmidt <philipp.schmidt@student.hpi.de>
2018-11-07 21:07:03 +0100

    yip

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,16 +5,25 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @author = Author.new
   end
 
   def create
-    @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+    @author = Author.new(author_params)
+    if (@author.save)
+      redirect_to @author
+    else
+      render 'new'
+    end
 
-    @author.save
-    redirect_to @author
   end
 
   def show
     @author = Author.find(params[:id])
+  end
+
+  private
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
   end
 end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -24,4 +24,15 @@ describe "New author page", type: :feature do
 
     find('input[type="submit"]').click
   end
+
+  it "should show validation errors" do
+    visit new_author_path
+
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+
+    find('input[type="submit"]').click
+
+    expect(page).to have_text('error')
+  end
 end
