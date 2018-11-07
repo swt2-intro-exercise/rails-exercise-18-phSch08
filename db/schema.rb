706abf76687cf3f1f7bf7cc5292f1e65e30a226f
Philipp Schmidt <philipp.schmidt@student.hpi.de>
2018-11-07 17:00:23 +0100

    Yihaa

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_220753) do
+ActiveRecord::Schema.define(version: 2018_11_07_154134) do
 
   create_table "authors", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "homepage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
