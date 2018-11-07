cbdf75f689685bcc194271ae54f765920d6c96d1
Philipp Schmidt <philipp.schmidt@student.hpi.de>
2018-11-07 20:13:07 +0100

    do stuff

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -1,9 +1,30 @@
-require 'rspec'
+require 'rails_helper'
 
-describe 'My behaviour' do
+describe 'Author index page', type: :feature do
 
-  it 'should do something' do
+  before :each do
+    @author = FactoryBot.create(:author)
+    visit authors_path
 
-    true.should == false
+  end
+
+  #it 'should have a table' do
+  #  expect(page).to have_table 'Name'
+  #end
+
+  it 'should list name of all authors' do
+    expect(page).to have_text 'Alan Turing'
+  end
+
+  it 'should list link to the authors homepage' do
+    expect(page).to have_link 'Homepage', href: 'http://wikipedia.org/Alan_Turing'
+  end
+
+  it 'should list link to author' do
+    expect(page).to have_link 'Show', href: author_path(@author)
+  end
+
+  it 'should have a link to create a new author' do
+    expect(page).to have_link 'New', href: new_author_path
   end
 end
